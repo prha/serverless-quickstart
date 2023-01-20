81adb86cd729ce064f068acc5b18065d13de37af
prha <1040172+prha@users.noreply.github.com>
2023-01-20 10:27:01 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+if __name__ == "__main__":
+    setup(
+        name="my_dagster_project",
+        packages=find_packages(exclude=["my_dagster_project_tests"]),
+        install_requires=[
+            "dagster",
+            "dagster-aws",
+            "dagster-cloud",
+        ],
+        extras_require={"dev": ["dagit"]},
+    )
